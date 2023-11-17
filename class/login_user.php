9c3bc030fd62b86c3d167db8383ff3e78f7ec7cc
Nguyen An Phu <anphu12t2@gmail.com>
2023-11-17 17:49:34 +0700

    add 1 time

---
+++ class/login_user.php
@@ -0,0 +1,11 @@
+<?php
+    include("class/clshome.php");
+	$p= new myhome();
+	if(!isset($_SESSION))
+	{
+		session_start();
+	}
+	if(isset($_SESSION['my_name']) && isset($_SESSION['my_pass'])){
+		$p->confirmLogin($_SESSION['my_name'],$_SESSION['my_pass']) ;
+	}
+?>
